f892a2e30fadf939d74677e7c499c6d39f3d6004
dBraga <douglasabraga@outlook.com>
2021-12-09 23:18:32 -0300

    refactor: add filter class on covid19-list

---
+++ src/app/components/covid19/covid19-filter.ts
@@ -0,0 +1,4 @@
+export class Covid19Filter {
+    Date: string
+    Country: string
+}
--- src/app/components/covid19/covid19-list/covid19-list.component.ts
+++ src/app/components/covid19/covid19-list/covid19-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Covid19Service } from '../services/covid19.service';
 import { Covid19 } from '../covid19.model'
-import { Subscription, zip } from 'rxjs';
+import { map, Observable, Subscription, zip } from 'rxjs';
+import { Covid19Filter } from '../covid19-filter';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-covid19-list',
@@ -10,35 +12,51 @@ import { Subscription, zip } from 'rxjs';
 })
 export class Covid19ListComponent implements OnInit, OnDestroy {
 
-  public covid19: Covid19[] = []
+  public covid19: Covid19[];
   private subscription: Subscription;
+  public filter: Covid19Filter = new Covid19Filter();
+  COUNTRIES_SLUG: string[] = ['mexico', 'south-africa', 'brazil', 'cuba', 'egypt', 'peru', 'canada']
 
   constructor(private covid19Service: Covid19Service) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.filter.Date = this.formatCurrentDate()
+    console.log(this.filter.Date)
+  }
 
-  onSearch() {
-    const multipleRequisition = zip(
-      this.covid19Service.getStatisticsCovid19ByCountry('mexico'),
-      this.covid19Service.getStatisticsCovid19ByCountry('south-africa'),
-      this.covid19Service.getStatisticsCovid19ByCountry('mozambique'),
-      this.covid19Service.getStatisticsCovid19ByCountry('cuba'),
-      this.covid19Service.getStatisticsCovid19ByCountry('egypt'),
-      this.covid19Service.getStatisticsCovid19ByCountry('peru'),
-      this.covid19Service.getStatisticsCovid19ByCountry('canada'))
-
-    this.subscription = multipleRequisition.subscribe({
-      next: (v) => {
-        console.log(v)
-        this.covid19.push(...v.map(item => item[0]))
+  onSearch(): void {
+    this.covid19 = [];
+    this.subscription = this.getMultipleRequisitions().subscribe({
+      next: (result: Covid19[]) => {
+        console.log(result)
+        if (result)
+          this.covid19 = result.filter(item => typeof item != 'undefined')
       },
       error: (e) => console.error(e),
       complete: () => console.info('complete')
     })
   }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe()
+  getMultipleRequisitions(): Observable<Covid19[]> {
+    return zip(
+      this.covid19Service.getStatisticsCovid19ByCountryByDate(this.COUNTRIES_SLUG[0], this.filter.Date),
+      this.covid19Service.getStatisticsCovid19ByCountryByDate(this.COUNTRIES_SLUG[1], this.filter.Date),
+      this.covid19Service.getStatisticsCovid19ByCountryByDate(this.COUNTRIES_SLUG[2], this.filter.Date),
+      this.covid19Service.getStatisticsCovid19ByCountryByDate(this.COUNTRIES_SLUG[3], this.filter.Date),
+      this.covid19Service.getStatisticsCovid19ByCountryByDate(this.COUNTRIES_SLUG[4], this.filter.Date),
+      this.covid19Service.getStatisticsCovid19ByCountryByDate(this.COUNTRIES_SLUG[5], this.filter.Date),
+      this.covid19Service.getStatisticsCovid19ByCountryByDate(this.COUNTRIES_SLUG[6], this.filter.Date))
+      .pipe(
+        map(arrayCovid19 => arrayCovid19.map(item => item[0]))
+      )
+  }
+
+  formatCurrentDate(): string {
+    return format(new Date, 'yyyy-MM-dd')
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) this.subscription.unsubscribe()
   }
 
 }
