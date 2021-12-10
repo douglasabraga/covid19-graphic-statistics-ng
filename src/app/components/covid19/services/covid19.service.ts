a45db33aef2485855c214da55371bcdc0f734d9f
dBraga <douglasabraga@outlook.com>
2021-12-09 23:21:23 -0300

    refactor: add function get statistics

--- src/app/components/covid19/services/covid19.service.ts
+++ src/app/components/covid19/services/covid19.service.ts
@@ -8,13 +8,18 @@ import { Covid19 } from '../covid19.model';
   providedIn: 'root'
 })
 export class Covid19Service {
-  
+
   private readonly API = environment.API
 
   constructor(private http: HttpClient) { }
 
-    getStatisticsCovid19ByCountry(country: string) : Observable<Covid19[]> {
-        return this.http
-        .get<Covid19[]>(this.API + `/country/${country}?from=2020-06-28T00:00:00Z&to=2020-06-28T00:00:01Z`)
-    }
+  getStatisticsCovid19ByCountry(country: string): Observable<Covid19[]> {
+    return this.http
+      .get<Covid19[]>(`${this.API}/country/${country}?from=2020-06-28T00:00:00Z&to=2020-06-28T00:00:01Z`)
+  }
+
+  getStatisticsCovid19ByCountryByDate(country: string, date: string): Observable<Covid19[]> {
+    return this.http
+      .get<Covid19[]>(`${this.API}/country/${country}?from=${date}T00:00:00Z&to=${date}T00:00:01Z`)
+  }
 }
