e4758d5f00b8e0378d8d3afac9b2deb7fca629c7
dBraga <douglasabraga@outlook.com>
2021-12-15 20:01:48 -0300

    fix: remove unuseds imports and propertys

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,21 +3,17 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastsContainer } from './toast/toasts-container.component';
-import { ToastService } from './toast/toast.service';
-import { ModuleWithProviders } from '@angular/compiler/src/core';
 
 @NgModule({
   declarations: [
-    ToastsContainer
+    ToastsContainer,
   ],
   imports: [
     CommonModule,
     FormsModule,
     NgbToastModule,
   ],
-  providers: [
-    ToastService
-  ],
+  providers: [ ],
   exports:[
     ToastsContainer
   ]
